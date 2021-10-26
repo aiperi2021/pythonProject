3a89fc9657da047c69802598fa910ad0b0ccaa52
aiperi2021 <aiperi.zhumaeva@cybertekschool.com>
2021-10-26 16:24:37 -0600

    tasks

---
+++ studentTask.py
@@ -0,0 +1,26 @@
+# Task 1:
+# Write a method that will create a student by parameter name and age
+
+student_name = "Jo"
+age = 14
+
+def student (student_name, age):
+    grade = input("What grade are you in ?")
+
+    yes_no = input("Are there more subjects?")
+    print(yes_no)
+
+    # subjects=[]
+    # if yes_no == "yes":
+    #     subjects.append(input("what subject?"))
+
+
+
+        # print(student_name, age, grade)
+
+
+
+student(student_name,age)
+
+# Task 2:
+# ADD a method that will ask INPUT from student what grade is he ?
---
+++ vending_machineTask.py
@@ -0,0 +1,12 @@
+# Task 1 Even vs Odd
+# Create a method which will take number as parameter 
+# and cheks if its even or odd, if even print "even" if odd "odd"
+
+# Task 2 Calculate wage 
+# Create a method which will take number (hour) and number (wage) as parameter 
+# and cheks if its below 40 hours (hour*wage) if hour above 40 (hour*(wage*1.5))
+# prints wage 
+
+# Task 3
+# Calculate monthly wage 
+
