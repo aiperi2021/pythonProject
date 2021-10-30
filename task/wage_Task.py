6756d1534d24e7910106f59c4536d1c046540238
aiperi2021 <aiperi.zhumaeva@cybertekschool.com>
2021-10-30 12:38:42 -0600

    new task

--- task/wage_Task.py
+++ task/wage_Task.py
@@ -10,6 +10,21 @@
 # Task 3
 # Calculate monthly wage and print total 
 
+# Task 4
 # The company made profit of 10000, in company works 7 people 
 # write a method that will take (profit) and (people) as parameter 
 # and find out how much each person gets after tax (tax is 20%)
+
+# Task 5 
+# Write a method that will take parameter as day_of_week and print:
+# if Monday, Tuesday  and sunny = go fishing,
+# if Monday, Tuesday  and cloudy = watch movie
+
+# if Friday, Wednesday, Thursday and sunny = go hiking
+# if Friday, Wednesday, Thursday and cloudy = study at home
+
+# if Weekend (Saturday, Sunday) = chill
+
+
+
+
